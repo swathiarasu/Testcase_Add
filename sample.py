687074dabbf4d78d803756fb9ad348a110462042
swathiarasu <43378414+swathiarasu@users.noreply.github.com>
2024-06-21 09:05:12 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,11 @@
+# Create two new variables and assign the values to be 10 and 6
+a = 10
+b = 6
+
+# Calculate the sum and the difference of the values and store them in variables Sum and dif respectively
+
+#------Add your code here----------------
+
+# Print the values of sum and dif
+print(Sum)
+print(dif)
